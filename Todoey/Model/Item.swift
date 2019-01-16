102c3df6ec6d8e85feb00fa755a9490f24b4a442
Phan Dang <phannhatdang@phans-macbook-air.local>
2019-01-16 20:20:24 +0700

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Phan Nhat Dang on 1/16/19.
+//  Copyright © 2019 Phan Nhat Dang. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title:String = ""
+    var done: Bool = false 
+}
